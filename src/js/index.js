f3cfbda889430fd903019bd4fbf3fc937283249b
Dominik Kurbiel <dominik.kurbiel@gmail.com>
2017-09-19 14:46:37 +0200

    Basic structure

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+ReactDOM.render(<div>lol</div>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './index.html',
+  filename: 'index.html',
+  inject: 'body',
+});
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: './js/index.js',
+  output: {
+    path: `${__dirname}/src/`,
+    filename: 'index.min.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: [
+            'react-html-attrs',
+            'transform-class-properties',
+            'transform-decorators-legacy',
+          ],
+        },
+      },
+    ],
+  },
+  plugins: [HtmlWebpackPluginConfig],
+};
